60c601b6d27860258cdb865779ff71f367e972ff
YogeshwaranAstamoort <YogeshwaranAstamoort@SYNCLAPN17725>
2022-01-25 15:53:06 +0530

    code changes

--- Azure_fileManager/BlazorApp1/Models/AzureFileProvider.cs
+++ Azure_fileManager/BlazorApp1/Models/AzureFileProvider.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Net;
 using System.Linq;
-using System.Text.RegularExpressions
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
